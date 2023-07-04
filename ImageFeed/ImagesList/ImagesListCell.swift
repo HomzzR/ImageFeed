e62427581aa2d0c225101d1d18f62b89db85a32f
HomzzR <homzzr@yandex.ru>
2023-07-04 20:01:50 +0300

    Группировка сущности

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Артем Калюжин on 04.07.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
