53bae1e5e4855f0574eb7852089cfb8f37413336
HomzzR <homzzr@yandex.ru>
2023-07-13 19:43:06 +0300

    Добавил класс профиля

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Артем Калюжин on 13.07.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
