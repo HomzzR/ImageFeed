b2ebbb7c9b0e0e15ecc70da31ad39962bae6f86c
HomzzR <homzzr@yandex.ru>
2023-07-14 20:00:50 +0300

    Добавил Single Image VC

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Артем Калюжин on 14.07.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+}
