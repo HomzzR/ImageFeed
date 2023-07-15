5c30ba84a74c001b57b6e826d3e8aaa3e3ba08e1
HomzzR <homzzr@yandex.ru>
2023-07-15 10:05:40 +0300

    Добавил ScrollView. Создал аутлет. Добавил делегат

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    // MARK: - Private properties & IBOutlet
     
     var image: UIImage! {
         didSet {
@@ -16,15 +17,31 @@ final class SingleImageViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
     }
+    
+    // MARK: - Private functions & IBAction
     
     @IBAction private func didTapBackButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
 }
+
+    // MARK: - Extensions
+extension SingleImageViewController: UITableViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+}
+
