b487578a3f020a6cfa595e147997a119052e77d1
HomzzR <homzzr@yandex.ru>
2023-07-29 19:32:23 +0300

    Настроил вид webView в сториборде

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import WebKit
 
 final class WebViewViewController: UIViewController {
-    
+    @IBOutlet private var webView: WKWebView!
+    @IBAction private func didTapBackButton(_ sender: Any?) {
+    }
     
 }
