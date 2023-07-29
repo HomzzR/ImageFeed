7ef01283e01340a980d1fc7b51048f417027026c
HomzzR <homzzr@yandex.ru>
2023-07-29 19:56:15 +0300

    Сделал авторизацию через webView

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -8,5 +8,39 @@
 import UIKit
 
 final class AuthViewController: UIViewController {
+    
+    // MARK: - Private properties & IBOutlet
+    
     private let ShowWebViewSegueIdentifier = "ShowWebView"
+    
+    // MARK: - Override
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowWebViewSegueIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(ShowWebViewSegueIdentifier)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    
+    // MARK: - Light content
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 }
+    // MARK: - Extenstion
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+}
+
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -8,9 +8,74 @@
 import UIKit
 import WebKit
 
+fileprivate let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+
+    // MARK: - Protocol
+
+protocol WebViewViewControllerDelegate: AnyObject {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
+}
+
 final class WebViewViewController: UIViewController {
+    
+    // MARK: - Private properties & IBOutlet
     @IBOutlet private var webView: WKWebView!
+    
+    weak var delegate: WebViewViewControllerDelegate?
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView.navigationDelegate = self
+        
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+           URLQueryItem(name: "client_id", value: accessKey),
+           URLQueryItem(name: "redirect_uri", value: redirectURI),
+           URLQueryItem(name: "response_type", value: "code"),
+           URLQueryItem(name: "scope", value: accessScope)
+         ]
+         let url = urlComponents.url!
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    // MARK: - Private functions
+    
     @IBAction private func didTapBackButton(_ sender: Any?) {
+        delegate?.webViewViewControllerDidCancel(self)
+    }
+}
+
+    // MARK: - Extensions
+
+extension WebViewViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationAction: WKNavigationAction,
+        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+    ) {
+        if let code = code(from: navigationAction) {
+            
+                decisionHandler(.cancel)
+          } else {
+                decisionHandler(.allow)
+            }
     }
     
+    private func code(from navigationAction: WKNavigationAction) -> String? {
+        if
+            let url = navigationAction.request.url,
+            let urlComponents = URLComponents(string: url.absoluteString),
+            urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: { $0.name == "code" })
+        {
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
 }
